cc139d133d6dbfe136788f61ca1ccca3d86c8756
Dawid Osiecki <deosiecki@gmail.com>
2024-08-20 21:39:33 +0200

    Add head image and media to portfolio model

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,11 +6,12 @@ from django.db import models
 class Portfolio(models.Model):
     title = models.CharField(max_length=100, blank=False)
     short_description = models.TextField(max_length=540, blank=False)
-    image = models.ImageField(upload_to='images/', blank=False)
+    header_image = models.ImageField(upload_to='images/', blank=False)
     live_page = models.URLField(max_length=200, blank=True)
     github_repo = models.URLField(max_length=200, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     detailed_description = models.TextField(max_length=2000, blank=True)
+    other_media = models.FileField(upload_to='media/', blank=True)
 
     def __str__(self):
         return self.title
